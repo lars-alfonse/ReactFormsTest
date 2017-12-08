95af1acef71fb50db7b984b2eeb256f19ff9e60a
lars-alfonse <lars.alfonse@gmail.com>
2017-12-07 20:52:26 -0600

    added navbar spacing to state

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -2,6 +2,15 @@ import React, { Component }from 'react';
 import { StyleSheet, Text, TextInput, Button, Alert, View, Image, AppRegistry, form } from 'react-native';
 
 export default class Header extends React.Component {
+	constructor(props){
+		super(props);
+		this.state={
+			navBarSpacing: 0,
+		}
+	}
+	componentDidMount(){
+		this.setState({navBarSpacing: Expo.Constants.statusBarHeight})
+	}
 	_buttonOne(){
 		Alert.alert("Button One");
 	}
@@ -13,7 +22,7 @@ export default class Header extends React.Component {
 	}
 	render(){
 		return(
-			<View style={{flex: 1, flexDirection: 'row', alignSelf: 'stretch', justifyContent:'space-between', backgroundColor: 'black', paddingTop: Expo.Constants.statusBarHeight}}>
+			<View style={{flex: 1, flexDirection: 'row', alignSelf: 'stretch', justifyContent:'space-between', backgroundColor: 'black', paddingTop: this.state.navBarSpacing}}>
 				<Button 
 					style={styles.headerButton}
             		onPress={this._buttonOne.bind(this)}
